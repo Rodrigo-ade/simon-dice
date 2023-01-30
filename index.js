9c0003a5fe95bd1b636d1ef416f847e87e5ab9d5
Rodrigo <zoccorodrigo@gmail.com>
2023-01-30 15:25:53 -0300

    Finaliza el simon dice

--- index.js
+++ index.js
@@ -5,27 +5,85 @@ let coloresUsuario = [];
 let numeroRonda = 1;
 
 function hacerTurno(){
+    document.querySelector("#comenzar").classList.add("oculto");
     bloquearInputUsuario();
     indicarTurnoMaquina();
     indicarNumeroRonda();
     agregarColorAleatorio();
-    pintarColoresMaquina();
-/*
-..desbloquearInputUsuario()
-indicar turno de usuario
-se elige un colorUsuario
-colorUsuario se agrega a coloresUsuario[]
-//Comparar coloresUsuario[posicion] con coloresMaquina[posicion]
-if no coinciden > error()
-if coinciden > 
-//if(coloresUsuarios.length === coloresMaquina.length){hacerTurno() Y AUMENTAR NUMERO DE RONDA} 
-else{seguir pidiendo colores de usuarios}
+    let RETRASO_TURNO_USUARIO = pintarColoresMaquina();
+
+    setTimeout(function(){
+        indicarTurnoUsuario();
+        desbloquearInputUsuario();
+    },RETRASO_TURNO_USUARIO);
+
 }
-*/
+
+function indicarTurnoUsuario(){
+    const $indicadorTurno = document.querySelector("#indicador-turno");
+    $indicadorTurno.textContent = "Es tu turno";
+    $indicadorTurno.classList.remove("alert-success");
+    $indicadorTurno.classList.add("alert-primary");
+}
+
+function desbloquearInputUsuario(){
+    let $colores = document.querySelectorAll(".color");
+    $colores.forEach(function($color){
+        $color.onclick = comprobarPatronUsuario;
+    });
+}
+
+function comprobarPatronUsuario(evento){
+    let $colorUsuario = evento.target;
+    pintarColor($colorUsuario);
+    coloresUsuario.push($colorUsuario);
+
+    if(coloresUsuario[coloresUsuario.length - 1] === coloresMaquina[coloresUsuario.length - 1]){
+        if(coloresUsuario.length === coloresMaquina.length){
+            numeroRonda ++;
+            coloresUsuario = [];
+            const RETRASO_NUEVO_TURNO = 1000;
+            
+            setTimeout(function(){
+                hacerTurno();
+            },RETRASO_NUEVO_TURNO);
+
+        }
+    }
+    else{
+        perdiste();
+    }
+}
+
+function pintarColor($color){
+    $color.style.opacity = 1;
+    const RETRASO_PINTADO = 750;
+
+    setTimeout(function(){
+        $color.style.opacity = 0.5;
+    }, RETRASO_PINTADO);
+}
+
+
+function perdiste(){
+    numeroRonda = 1;
+    coloresMaquina = [];
+    coloresUsuario = [];
+    bloquearInputUsuario();
+    document.querySelector("#comenzar").classList.remove("oculto");
+
+    const $indicadorTurno = document.querySelector("#indicador-turno");
+    $indicadorTurno.textContent = "¡Perdiste! Toca comenzar para volver a jugar";
+    $indicadorTurno.classList.remove("alert-primary");
+    $indicadorTurno.classList.add("alert-warning");
 }
 
 function bloquearInputUsuario(){
-    //Completar
+    let $colores = document.querySelectorAll(".color");
+    $colores.forEach(function($color){
+        $color.onclick = function(){
+        };
+    });
 }
 
 function indicarTurnoMaquina(){
@@ -33,7 +91,6 @@ function indicarTurnoMaquina(){
     $indicadorTurno.textContent = "Es el turno de la máquina";
     $indicadorTurno.classList.remove("alert-warning");
     $indicadorTurno.classList.add("alert-success");
-
 }
 
 function indicarNumeroRonda(){
@@ -49,7 +106,8 @@ function agregarColorAleatorio(){
 
 function pintarColoresMaquina(){
     coloresMaquina.forEach(function($color,index){
-        let retrasoPorColor = (index + 1) * 1000;
+        let retrasoPorColor = (index + 1) * 1500;
+        const RETRASO_EXTRA = 900;
 
         setTimeout(function(){
             $color.style.opacity = 1;
@@ -57,6 +115,8 @@ function pintarColoresMaquina(){
 
         setTimeout(function(){
             $color.style.opacity = 0.5;
-        },retrasoPorColor + 500);
+        },retrasoPorColor + RETRASO_EXTRA);
     });
+
+    return (coloresMaquina.length + 1) * 1500;
 }
